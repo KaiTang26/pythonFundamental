9b87b5e9a6558c2888392e51b3ae829c9b7800e8
KaiTang26 <16kt16@queensu.ca>
2019-01-02 20:03:57 -0500

    some example for day one

--- FunDay1.py
+++ FunDay1.py
@@ -1,2 +1,59 @@
-import math
+from math import factorial as fac
+
+n = 5
+
+k = 3
+
+result = fac(n)//(fac(k)*fac(n-k))
+
+print(result)
+
+print(int("10000", 2))
+
+a = None
+
+print( a is None)
+
+if True:
+    print("this True")
+
+h = 42
+
+if h > 50:
+    print("h is greater than 50")
+else:
+    print("h is less than 50")
+
+c = 5
+
+while c >0:
+    print(c)
+    c -= 1
+
+ # while True:
+ #    response = input()
+ #    if int(response) % 7 == 0:
+ #        print("good guess")
+ #        break
+
+# colection in python: str, bytes, list dict
+
+aString = """ hadjfdkfs
+dfkdfdfkdfjjf"""
+
+bString = "kdfkdfjdkfdkfjdf \n dkfdfjdklfjd"
+
+print(aString)
+
+print(bString.capitalize())
+
+print(aString.encode("utf-8"))
+
+f = [1,2,3,]
+
+print(f[1])
+
+
+phoneList = {"kai": "6138764252", "lala": "84379385"}
+print(phoneList["kai"])
 
